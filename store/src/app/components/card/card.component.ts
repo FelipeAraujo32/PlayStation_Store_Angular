ec837f9597647309a68023f2c531b614e058af49
FelipeAraujo32 <felipecafsx@gmail.com>
2024-04-26 16:19:24 -0300

    Deixando Cards Auto

--- store/src/app/components/card/card.component.ts
+++ store/src/app/components/card/card.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CardLabelComponent } from "./card-label/card-label.component";
+import { CardPricingComponent } from "./card-pricing/card-pricing.component";
 
 @Component({
     selector: 'app-card',
     standalone: true,
     templateUrl: './card.component.html',
     styleUrl: './card.component.css',
-    imports: [CardLabelComponent]
+    imports: [CardLabelComponent, CardPricingComponent]
 })
 export class CardComponent {
 
+    @Input()
+    gamerCover:string = ""
+
+    @Input()
+    gameLabel:string=""
+
+    @Input()
+    gameType:string = "Digital PS4"
+    
+    @Input()
+    gamePrice:string = "RS 399,99"
 }
